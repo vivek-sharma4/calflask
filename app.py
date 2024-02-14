ce1f2c52f212b2eb70a4a43ec695c746486e5083
Vivek Sharma <viveksharma432002@gmail.com>
2024-02-14 22:20:44 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,28 @@
+from flask import Flask,request,render_template
+
+app=Flask(__name__)
+
+@app.route('/')
+def welcome():
+    return "Welcomr to the Flask"
+
+@app.route('/cal',methods=["GET"])
+def math_operator():
+    operation=request.json["operation"]
+    number1=request.json["number1"]
+    number2=request.json["number2"]
+    
+    if operation=="add":
+        result=number1+number2
+    elif operation=="multiply":
+        result=number1+number2
+    elif operation=="division":
+        result=number1+number2
+    else:
+        result=number1+number2
+    return result
+
+print(__name__)
+
+if __name__=='__main__':
+    app.run()
